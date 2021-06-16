291ef34843aff7e0a7e612dbc9e531c63f6b5fc9
Md. Omar Faruk <iamomar022@gmail.com>
2021-06-16 19:27:50 +0600

    Redux, Reducer, Actions

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,34 +3,51 @@ import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product' 
 // import products from '../products'  // Static data
 
-import axios from 'axios'
+// import axios from 'axios'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../actions/productActions'
+
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 function HomeScreen() {
-    const [products, setProducts] = useState([])
+    // const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { error, loading, products } = productList
 
     useEffect(() => {
         // console.log('Use Effect Triggered');
-        async function fetchProducts(){
-            const { data } = await axios.get('/api/products/')
-            setProducts(data)
-        }
+        // async function fetchProducts(){
+        //     const { data } = await axios.get('/api/products/')
+        //     setProducts(data)
+        // }
 
-        fetchProducts()
+        // fetchProducts()
+        dispatch(listProducts())
 
-    }, [])
+    }, [dispatch])
 
     return (
         <div>
             <h1>Latest Products</h1>
-            <Row>
-                {/* Loop To View Product */}
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        {/* <h3>{product.name}</h3> */}
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+
+            {
+                loading ? <Loader/>
+                : error ? <Message variant='danger'>{ error }</Message>
+                :
+                <Row>
+                    {/* Loop To View Product */}
+                    {products.map(product => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            {/* <h3>{product.name}</h3> */}
+                            <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>
+            }
+
         </div>
     )
 }
