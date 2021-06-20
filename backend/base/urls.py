e96d57806a11000563c5be83f86d70d8f5522d45
Md. Omar Faruk <iamomar022@gmail.com>
2021-06-20 14:27:52 +0600

    Protected Routes

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -9,7 +9,10 @@ urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name='routes'),
+
     path('users/profile/', views.getUserProfile, name='users-profile'),
+    path('users/', views.getUsers, name='users'),
+
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>/', views.getProduct, name='product'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import JsonResponse
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 # from . products import products  # Static data
@@ -57,16 +59,26 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)   # If single product then set False.
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)  # Single use that's why False
     return Response(serializer.data)
 
 
 @api_view(['GET'])
-def getUserProfile(request):
-    user = request.user
-    serializer = UserSerializer(user, many=False)  # Single use that's why False
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)   # If single product then set False.
     return Response(serializer.data)
 
 
