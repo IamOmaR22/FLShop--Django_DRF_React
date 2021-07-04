46327b0f3d6b4d86bb1bffd8242c794134241a27
IamOmaR22 <iamomar022@gmail.com>
2021-07-04 13:23:06 +0600

    Merging React Files With Django Project

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),  # This path will render the React project.
     # path('api/', include('base.urls')),
     path('api/products/', include('base.urls.product_urls')),  # Here urls is a folder.
     path('api/users/', include('base.urls.user_urls')),  # Here urls is a folder.
